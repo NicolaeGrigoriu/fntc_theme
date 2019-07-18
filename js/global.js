df8d20135cbca1932782c0970a90d1de2af1fdb3
Nicolae Grigoriu <ng@inlead.dk>
2019-07-18 22:44:36 +0300

    First commit.

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fntc = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
